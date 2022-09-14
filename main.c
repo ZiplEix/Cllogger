3d651a6a975d5dde11e7817ce8f651f6800267dd
Baptiste Leroyer <leroyerbaptiste@gmail.com>
2022-09-14 10:42:37 +0200

    :white_check_mark: test github action 2
    just modify a little the main.c file to trigger the GitHub action to test it

--- main.c
+++ main.c
@@ -8,7 +8,7 @@
 
 int main(int argc, char const *argv[])
 {
-    // just a little comment to test the github action
+    // another little comment to test github action
     logger("une info");
     logger_warning("un warning");
     logger_error("une erreur");
