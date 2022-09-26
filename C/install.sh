28e06cd94627b1ae470b54fc9560c6b983235c5f
Baptiste Leroyer <86067803+ZiplEix@users.noreply.github.com>
2022-09-26 13:40:45 +0200

    :memo:

--- C/install.sh
+++ C/install.sh
@@ -1,13 +1,13 @@
-echo "Create cllogger folder ..."
+echo "Create sulogger folder ..."
 echo "Create include folder ..."
-mkdir -p cllogger/include
+mkdir -p sulogger/include
 echo "Copy header files ..."
-cp -r include/cllogger.h cllogger/include
+cp -r include/sulogger.h sulogger/include
 echo "Create src folder ..."
-mkdir -p cllogger/src
+mkdir -p sulogger/src
 echo "Compiling source files ..."
 gcc -c src/*.c
 echo "Compiling lib ..."
-ar -rc cllogger/src/cllogger.a cllogger.o logger_reset.o write_log.o
+ar -rc sulogger/src/sulogger.a sulogger.o logger_reset.o write_log.o
 echo "Delete useless file ..."
 rm *.o
