ed1f71f161a96e9eebda789f7cdc771e991bc096
Baptiste <baptiste.leroyer@epitech.eu>
2022-10-14 17:50:19 +0200

    :art: improve structure of the code

--- JS/test.js
+++
@@ -1,6 +0,0 @@
-const Logger = require('./logger')
-
-Logger.log('a log message', true);
-Logger.debug('a debug message', true);
-Logger.warning('a warning message', true);
-Logger.error('an error message', true);
